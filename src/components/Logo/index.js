9d114bfd48c3a13a66b0bbc373fe9e06836f8346
Teddykavooh <teddykavooh@gmail.com>
2023-03-28 09:38:55 +0300

    Disable drawing svg feature

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,25 +3,24 @@ import LogoS from '../../assets/images/logo-a-big.png'
 import { useRef } from 'react'
 import { useEffect } from 'react'
 import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 
 const Logo = () => {
   const bgRef = useRef()
-  const outlineLogoRef = useRef()
+  // const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
+  // Disable svg drawing`
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
+    // gsap.registerPlugin(DrawSVGPlugin)
+    gsap.timeline().to(bgRef.current, {
+      duration: 1,
+      opacity: 1,
+    })
+    // .from(outlineLogoRef.current, {
+    //   drawSVG: 0,
+    //   duration: 20,
+    // })
     gsap.fromTo(
       solidLogoRef.current,
       {
@@ -29,21 +28,16 @@ const Logo = () => {
       },
       {
         opacity: 1,
-        delay: 4,
+        delay: 3,
         duration: 4,
       }
     )
   }, [])
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     solidLogoRef.current
-  //   }, 1000);
-  // }, [])
-
   return (
     <div className="logo-container" ref={bgRef}>
       <img className="solid-logo" src={LogoS} alt="AK" ref={solidLogoRef} />
+      {/* Disable svg drawing */}
       {/* <svg
         width="559pt"
         height="897pt"
