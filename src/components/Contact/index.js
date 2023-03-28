774cafe414a261cea7902cd97df7276296a9b53a
Teddykavooh <teddykavooh@gmail.com>
2023-03-28 08:54:39 +0300

    Update contact map zoom value

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -97,7 +97,7 @@ const Contact = () => {
           <span>teddykavooh@gmail.com</span>
         </div>
         <div className="map-wrap">
-          <MapContainer center={position} zoom={10} scrollWheelZoom={false}>
+          <MapContainer center={position} zoom={6.4} scrollWheelZoom={false}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
