9ef193403981f22fac1ef96022782b983b3046e4
Teddykavooh <teddykavooh@gmail.com>
2024-02-11 02:16:32 +0300

    UI Update

--- src/components/ComingSoon/index.js
+++ src/components/ComingSoon/index.js
@@ -6,6 +6,29 @@ const ComingSoon = () => {
             <div className='coming_soon_body'>
                 <h2>COMING SOON, THICK & FAST !!!</h2>
             </div>
+            <p className='p_section'>
+                <h3>In the meanwhile, check out my wits :)</h3>
+                <ul>
+                    <h3>Scripting:</h3>
+                    <li><a href="https://github.com/Teddykavooh/Python-Scripts">Sample Scripts</a></li>
+                    <li><a href='https://github.com/Teddykavooh/Personal-Scripts'>Personalized Scripts</a></li>
+                </ul>
+                <ul>
+                    <h3>Web Development:</h3>
+                    <li><a href='https://antonykavoo-portfolio.vercel.app/'>Where you are</a></li>
+                    <li><a href='https://teddykavooh.github.io/Disney-Clone/'>Disney Clone</a></li>
+                    <li><a href='https://teddykavooh.github.io/Interpeace-Project/'>Interpeace</a></li>
+                    <li><a href='https://teddykavooh.github.io/Emir/'>Travel Agency</a></li>
+                    <li><a href='https://teddykavooh.github.io/ridemyway.github.io/'>Car Pooling</a></li>
+                    <li><a href='https://teddykavooh.github.io/Coffee_Machine/'>Another Coffee Machine</a></li>
+                </ul>
+                <ul>
+                    <h3>Android Development:</h3>
+                    <li><a href='https://github.com/Teddykavooh/PrismApp'>PrismApp</a></li>
+                    <li><a href='https://github.com/Teddykavooh/PrismAppBT'>PrismAppBT</a></li>
+                    <li><a href='https://github.com/Teddykavooh/BetIQPro'>BetIQPro</a></li>
+                </ul>
+            </p>
         </>
     )
 }
