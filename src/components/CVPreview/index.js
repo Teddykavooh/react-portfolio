71248e1c465f431d5950fd0ca8dbe1fe2e31b0c4
Teddykavooh <teddykavooh@gmail.com>
2025-06-18 00:50:17 +0300

    Change CV preview from cloud to local

--- src/components/CVPreview/index.js
+++ src/components/CVPreview/index.js
@@ -2,6 +2,7 @@ import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
+import CVPDF from '../../../public/Antony_Kavoo_CV.pdf'
 
 const CVPreview = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -26,7 +27,8 @@ const CVPreview = () => {
           </div>
           <div className="cv-preview">
             <iframe 
-                src="https://drive.google.com/file/d/1Ygax5bfbYgz2lVHaEo4QIr61ca0kIeW6/view" 
+                // src="https://drive.google.com/file/d/1Ygax5bfbYgz2lVHaEo4QIr61ca0kIeW6/view"
+                src={CVPDF}
                 width="100%" 
                 height="600"
                 allow="autoplay">
@@ -34,7 +36,8 @@ const CVPreview = () => {
             <p>
                 <a 
                     href="https://drive.google.com/file/d/1Ygax5bfbYgz2lVHaEo4QIr61ca0kIeW6/view?usp=drive_link"
-                    class="inline-block mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                    // class="inline-block mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                    className="cv-link"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
